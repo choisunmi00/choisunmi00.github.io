3bdd06f16d77a6571ec6dded5f56222fdb6436d7
choisunmi00 <keunangyung01@naver.com>
2024-12-18 18:59:59 +0900

    modify popup

--- _javascript/modules/components/img-popup.js
+++ _javascript/modules/components/img-popup.js
@@ -42,16 +42,16 @@ export function imgPopup() {
     touchNavigation: true,
     loop: true,
     zoomable: true,
-    width: '90vw', // 뷰포트 너비의 90%
-    height: 'auto', // 높이는 원본 비율에 맞춰 자동 조정
-    autoplayVideos: true, // 비디오 자동 재생 (선택 사항)
+    width: '90vw',
+    height: 'auto',
     onOpen: () => {
-      document.body.style.overflow = 'hidden'; // 팝업 열릴 때 스크롤 비활성화
+      document.body.style.overflow = 'hidden'; // 스크롤 비활성화
     },
     onClose: () => {
-      document.body.style.overflow = ''; // 팝업 닫힐 때 스크롤 활성화
+      document.body.style.overflow = ''; // 스크롤 활성화
     },
   });
+  
   
   if (hasDualImages && Theme.switchable) {
     let reverse = null;
