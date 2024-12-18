530e96fc9b2d8b25b22a2f946a27de5feb9770b2
choisunmi00 <keunangyung01@naver.com>
2024-12-18 18:57:50 +0900

    popup

--- _javascript/modules/components/img-popup.js
+++ _javascript/modules/components/img-popup.js
@@ -36,15 +36,23 @@ export function imgPopup() {
     selector = darkImages;
   }
 
-  let current = GLightbox({ 
-    selector: `${selector}`,
+  // let current = GLightbox({ selector: `${selector}` });
+  let current = GLightbox({
+    selector: '.popup',
     touchNavigation: true,
     loop: true,
-    width: '80%',
-    height: 'auto',
-    zoomable: true, 
+    zoomable: true,
+    width: '90vw', // 뷰포트 너비의 90%
+    height: 'auto', // 높이는 원본 비율에 맞춰 자동 조정
+    autoplayVideos: true, // 비디오 자동 재생 (선택 사항)
+    onOpen: () => {
+      document.body.style.overflow = 'hidden'; // 팝업 열릴 때 스크롤 비활성화
+    },
+    onClose: () => {
+      document.body.style.overflow = ''; // 팝업 닫힐 때 스크롤 활성화
+    },
   });
-
+  
   if (hasDualImages && Theme.switchable) {
     let reverse = null;
 
