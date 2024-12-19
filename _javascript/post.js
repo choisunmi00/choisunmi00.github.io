d032c1460437466c61ca5346b71c6e42204ced5f
choisunmi00 <keunangyung01@naver.com>
2024-12-19 10:41:22 +0900

    Update post.js

--- _javascript/post.js
+++ _javascript/post.js
@@ -18,3 +18,10 @@ initClipboard();
 initTopbar();
 loadMermaid();
 basic();
+
+document.addEventListener("DOMContentLoaded", function() {
+  const lazyImages = document.querySelectorAll("img[loading='lazy']");
+  lazyImages.forEach((img) => {
+    img.src = img.dataset.src;
+  });
+});
